51edbeb25b5ca456b924a7bd1d690cd8293e2b3d
Colton Colcleasure <colton.colcleasure@copart.com>
2018-11-05 05:30:49 -0600

    Add tests, update pkg, add prettier, add README.

--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
 const stringify = target => {
-  return typeof target === "string" ? target : JSON.stringify(target);
-};
+  return typeof target === 'string'
+    ? target
+    : typeof target === 'undefined'
+      ? 'undefined'
+      : JSON.stringify(target)
+}
 
 const parse = target => {
   try {
-    return JSON.parse(target);
+    return JSON.parse(target)
   } catch (error) {
-    return target;
+    return target
   }
-};
+}
 
 module.exports = {
   stringify,
   parse
-};
+}
